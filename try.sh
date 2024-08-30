367915679ccc76942acf926d120f7da0f7b5c8fc
Samer <CS.Samer21@gmail.com>
2024-08-31 00:42:58 +0300

    streamlit

--- try.sh
+++ try.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-curl -X POST "http://127.0.0.1:8000/predict" -H "Content-Type: application/json" \
+curl -X POST "https://labtop.onrender.com/predict" -H "Content-Type: application/json" \
 -d '{"Swimming_Pool":1 , "Room_Service":1 , "Fitness_Centre":0 , "Airport_Shuttle":0 , "Highspeed_Internet":1 , "Airconditioning":1}'
