81791cfa3f2ae203c4e6614031c197d14444543f
Samer <CS.Samer21@gmail.com>
2024-08-31 00:49:36 +0300

    test Streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,18 +2,18 @@ import streamlit as st
 import requests
 
 # Set the URL for the FastAPI app
-url = "http://127.0.0.1:8000/predict"  # Update with your FastAPI URL
+url = "https://labtop.onrender.com/predict"  # Updated URL
 
 # Create the Streamlit app
 st.title("Hotel Amenities Clustering")
 
-# Input fields for the user to provide data, with values 0 (absent) or 1 (present)
-swimming_pool = st.slider("Is there a Swimming Pool?", 0, 1, 0)
-room_service = st.slider("Is there Room Service?", 0, 1, 0)
-fitness_centre = st.slider("Is there a Fitness Centre?", 0, 1, 0)
-airport_shuttle = st.slider("Is there an Airport Shuttle?", 0, 1, 0)
-highspeed_internet = st.slider("Is there Highspeed Internet?", 0, 1, 0)
-airconditioning = st.slider("Is there Airconditioning?", 0, 1, 0)
+# Input fields for the user to provide data using select boxes
+swimming_pool = st.selectbox("Is there a Swimming Pool?", [0, 1], index=0)
+room_service = st.selectbox("Is there Room Service?", [0, 1], index=0)
+fitness_centre = st.selectbox("Is there a Fitness Centre?", [0, 1], index=0)
+airport_shuttle = st.selectbox("Is there an Airport Shuttle?", [0, 1], index=0)
+highspeed_internet = st.selectbox("Is there Highspeed Internet?", [0, 1], index=0)
+airconditioning = st.selectbox("Is there Airconditioning?", [0, 1], index=0)
 
 # Prepare the data to be sent as JSON
 data = {
